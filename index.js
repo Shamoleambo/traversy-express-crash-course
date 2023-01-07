95734bc4fcca3bb08409f407082a5c1455d30c96
Tiago Gomes <tidgomes@gmail.com>
2023-01-07 10:56:24 -0300

    send json as response

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const path = require('path')
 
 const app = express()
 
+const members = [
+  {
+    id: 1,
+    name: 'Mano',
+    email: 'mano@mail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Truta',
+    email: 'truta@mail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Manolo',
+    email: 'manolo@mail.com',
+    status: 'active'
+  }
+]
+
+app.get('/api/members', (req, res) => {
+  res.json(members)
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000
