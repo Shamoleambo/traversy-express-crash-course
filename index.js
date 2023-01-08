e11f1ff628a2df615fa3bb9ebf0121f53e7b2d43
Tiago Gomes <tidgomes@gmail.com>
2023-01-08 10:28:33 -0300

    extract logger middleware to its own middleware file

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const path = require('path')
 const members = require('./Members')
+const logger = require('./middleware/logger')
 
 const app = express()
-
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
-  next()
-}
 
 app.use(logger)
 
 app.get('/api/members', (req, res) => res.json(members))
-
 
 app.use(express.static(path.join(__dirname, 'public')))
 
