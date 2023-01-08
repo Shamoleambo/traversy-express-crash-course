f65a72e14125282bc72805f338c214c149d546f5
Tiago Gomes <tidgomes@gmail.com>
2023-01-08 10:41:28 -0300

    get a single method request

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.use(logger)
 
 app.get('/api/members', (req, res) => res.json(members))
 
+app.get('/api/members/:id', (req, res) => {
+  const member = members.find(member => member.id === parseInt(req.params.id))
+  if (!member) {
+    res.status(404)
+    res.json({ message: 'No member found' })
+  } else {
+    res.status(200)
+    res.json(member)
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000
