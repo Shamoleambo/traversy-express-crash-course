68bdc3070fe6322777ede453cd22c7ca6b63b460
Tiago Gomes <tidgomes@gmail.com>
2023-01-08 10:16:35 -0300

    create Members model

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Mano',
+    email: 'mano@mail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Truta',
+    email: 'truta@mail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Manolo',
+    email: 'manolo@mail.com',
+    status: 'active'
+  }
+]
+
+module.exports = members
--- index.js
+++ index.js
@@ -2,31 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const members = require('./Members')
 
-const members = [
-  {
-    id: 1,
-    name: 'Mano',
-    email: 'mano@mail.com',
-    status: 'active'
-  },
-  {
-    id: 2,
-    name: 'Truta',
-    email: 'truta@mail.com',
-    status: 'inactive'
-  },
-  {
-    id: 3,
-    name: 'Manolo',
-    email: 'manolo@mail.com',
-    status: 'active'
-  }
-]
+app.get('/api/members', (req, res) => res.json(members))
 
-app.get('/api/members', (req, res) => {
-  res.json(members)
-})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
