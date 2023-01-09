eac9de7ea7dbf705b7ff59ca7619d105879e44d4
Tiago Gomes <tidgomes@gmail.com>
2023-01-09 09:36:40 -0300

    create new member route

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const uuid = require('uuid')
 const members = require('../../Members')
 
 router.get('/', (req, res) => res.json(members))
@@ -13,6 +14,22 @@ router.get('/:id', (req, res) => {
       .json({ message: `No member with the id of ${req.params.id}` })
   } else {
     res.status(200).json(member)
+  }
+})
+
+router.post('/', (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  }
+
+  if (!req.body.name || !req.body.email) {
+    res.status(400).json({ message: 'Not a valid user' })
+  } else {
+    members.push(newMember)
+    res.json(members)
   }
 })
 
